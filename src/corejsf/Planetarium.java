d8c37c65227922fa41d6caf52abb331c7e209ac5
Ngoc Tuyen <buingoctuyen1001@gmail.com>
2018-10-17 17:36:37 +0700

    facelets example

---
+++ src/corejsf/Planetarium.java
@@ -0,0 +1,21 @@
+package corejsf;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class Planetarium implements Serializable {
+	private String selectedPlanet;
+
+	public String getSelectedPlanet() {
+		return selectedPlanet;
+	}
+
+	public String changePlanet(String newValue) {
+		selectedPlanet = newValue;
+		return selectedPlanet;
+	}
+}
